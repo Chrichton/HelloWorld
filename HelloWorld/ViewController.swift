f79a4e22dbc43271aaf713f953ec6df439b77015
Heiko Goes <heikogoes@Heikos-Mac-mini.local>
2019-05-04 16:13:26 +0200

    newTry

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 1 scond  try second merge  Do any additional setup after loading the view, typically from a nib.
+        // 2.2.2  Do any additional setup after loading the view, typically from a nib.
     }
 
 
