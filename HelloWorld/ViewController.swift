43cb4d2b040457437bdf06d7e2c30bfdfac72285
Heiko Goes <heikogoes@Heikos-Mac-mini.local>
2019-05-04 14:50:10 +0200

    1.1.1.1

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 1.1.2  Do any additional setup after loading the view, typically from a nib.
+        // 1.1.1.1 Rebase  Do any additional setup after loading the view, typically from a nib.
     }
 
 
