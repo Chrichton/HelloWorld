bc00d7457f5909a96443ec60ff1eeef8ae08dfeb
Heiko Goes <heikogoes@Heikos-Mac-mini.local>
2019-05-04 16:10:58 +0200

    1.1.1.1merge2

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // merge 1 new try  Do any additional setup after loading the view, typically from a nib.
+        // merge 1.1 new try  Do any additional setup after loading the view, typically from a nib.
     }
 
 
