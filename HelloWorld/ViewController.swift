f8bada37df8a1cb2b74eedd48638ca7caf916564
Heiko Goes <heikogoes@Heikos-Mac-mini.local>
2019-05-04 16:10:20 +0200

    newTry2

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 1 new try  Do any additional setup after loading the view, typically from a nib.
+        // 1 scond  try  Do any additional setup after loading the view, typically from a nib.
     }
 
 
